d2790d7f9270841041ef8937ce5af84ccfa1f452
Prakharjainprince <prakharjain995@gmail.com>
2020-10-21 18:06:48 +0530

    Hello file

---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <React.Fragment>
+            <h1>Hello to all</h1>
+        </React.Fragment>
+    );
+};
+export default Hello;
